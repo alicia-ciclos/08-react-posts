08dba13e587c24296d256eaec927536aff323c54
ALICE WYAN GARCIA MARTIN <alicew.garmar@educa.jcyl.es>
2025-02-04 13:47:52 +0100

    Comentarios

---
+++ src/Comentarios.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react"
+
+const Comentarios = ({ postId }) => {
+    const [comentarios, setComentarios] = useState([])
+    const [cargado, setCargado] = useState(false)
+
+    async function onToggle(e) {
+        if(!cargado) {
+            const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
+            const data = await res.json()
+            setComentarios(data)
+            setCargado(true);
+        }
+    }
+
+    return <details onToggle={onToggle}>
+        <summary>Comentarios</summary>
+        {comentarios.map(comentario => <div key={comentario.id}>
+            <h3>{comentario.name}</h3>
+            <p>{comentario.body}</p>
+        </div>)}
+    </details>
+}
+
+export default Comentarios
--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,19 +1,39 @@
 import { useState, useEffect } from 'react'
+import Comentarios from './Comentarios'
 
 function Posts() {
     const [posts, setPosts] = useState([])
     const [loading, setLoading] = useState(true)
+    const [users, setUsers] = useState([])
     
     useEffect(() => {
         async function fetchPosts() {
             const res = await fetch('https://jsonplaceholder.typicode.com/posts')
             const postList = await res.json()
             setPosts(postList)
-            setLoading(false)
         }
 
         fetchPosts()
     }, [])
+
+    useEffect(() => {
+        async function fetchUserNames() {
+            const uniqueUserIds = [...new Set(posts.map(post => post.userId))]
+            const userNamePromises = uniqueUserIds.map(uid =>
+                fetch(`https://jsonplaceholder.typicode.com/users/${uid}`)
+                .then(res => res.json())
+            )
+            const authors = await Promise.all(userNamePromises);
+            console.log("AUTHORS")
+            console.log(authors)
+            const authorNames = authors.map(user => user.name);
+            console.log(authorNames)
+            setUsers(authors)
+            setLoading(false)
+        }
+
+        fetchUserNames()
+    }, [posts])
 
     return (
         <>
@@ -22,7 +42,9 @@ function Posts() {
                 posts.map(post =>
                     <div className="post" key={post.id}>
                         <h1>{post.title}</h1>
+                        <p>{users[post.userId - 1]?.name}</p>
                         <p>{post.body}</p>
+                        <Comentarios postId={post.id} />
                     </div>
                 )
             }
